4990342160f013a20d594d2cd50a2e27bb01e6a8
Karol Boś <webkarolbos@gmail.com>
2023-02-08 13:20:20 +0100

    Trigger deploy

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,9 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <p>
+          Workflow Test
+        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
